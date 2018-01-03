27a9b8605a2aaeafa3dd9274045d2b19b9f7bd37
Borge Rodsjo <borge.rodsjo@bouvet.no>
2018-01-03 23:05:29 +0100

    feat: initial commit

---
+++ Jaktapp/MainActivity.cs
@@ -0,0 +1,20 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace Jaktapp
+{
+    [Activity(
+        MainLauncher = true, 
+        Name = "jaktapp.no.mainactivity",
+        Label = "@string/app_name",
+        ScreenOrientation = Android.Content.PM.ScreenOrientation.Portrait)]
+    public class MainActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            
+            SetContentView(Resource.Layout.Main);
+        }
+    }
+}
