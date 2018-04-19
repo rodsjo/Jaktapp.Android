c072153de5a10351899667ca8cab08062251b1d2
Borge Rodsjo <borge.rodsjo@bouvet.no>
2018-04-19 14:34:53 +0200

    Added Test project with a simple test of the Login screen. The test uses a Release apk

---
+++ Jaktapp.Test/AppInitializer.cs
@@ -0,0 +1,26 @@
+﻿using Xamarin.UITest;
+
+namespace Jaktapp.Tests
+{
+    public class AppInitializer
+    {
+        public static IApp StartApp(Platform platform)
+        {
+            if (platform == Platform.Android)
+            {
+                // relative path from 'Jaktapp.Test/bin/<Debug/Release>/Jaktapp.Test.dll'
+                // The uploaded apk to appcenter shall be a Release-build, to be as close to prod-app as possible
+                return ConfigureApp
+                    .Android
+                    .ApkFile("../../../Jaktapp/bin/Release/jaktlag.rodsjodev.no.apk")
+                    .EnableLocalScreenshots()
+                    .StartApp();
+            }
+
+            return ConfigureApp
+                .iOS
+                .StartApp();
+        }
+    }
+}
+
---
+++ Jaktapp.Test/StartupTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using Jaktapp.Tests;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+using Xamarin.UITest.Android;
+
+namespace Jaktapp.Test
+{
+    [TestFixture(Platform.Android)]
+    public class StartupTests
+    {
+        private IApp _app;
+        private readonly Platform _platform;
+        private readonly TimeSpan _timeout = TimeSpan.FromSeconds(20);
+
+        public StartupTests(Platform platform)
+        {
+            _platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            _app = AppInitializer.StartApp(_platform);
+        }
+
+        [Test]
+        public void App_should_start_and_show_login()
+        {
+            //Arrange
+            //https://docs.microsoft.com/en-us/appcenter/test-cloud/uitest/#using-the-repl
+            //_app.Repl();
+
+            //Act
+            var loginBtn = _app.WaitForElement(v => v.Marked("LoginBtn"), "Timed out waiting for favorites tab", _timeout);
+
+            //Assert
+            Assert.IsTrue(!string.IsNullOrEmpty(loginBtn[0].Text));
+
+            _app.Screenshot("Login screen");
+        }
+    }
+}
+
